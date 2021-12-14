90ac3e31328118e9d01ef6521ec0df7f8f6c9a27
John Payne <johpayne@geico.com>
2021-12-14 08:39:31 -0500

    node callbacks and error handling

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const rect = require('./reactangle');
 
+/** 
 function solveRect(l, w) {
     console.log(`Solving for rectangle with dimensions: ${l}, ${w}`);
 
@@ -10,7 +11,21 @@ function solveRect(l, w) {
         console.log(`Perimeter of rectangle: ${rect.perimeter(l, w)}`);
     }
 }
+*/
 
+function solveRect(l, w){
+    console.log(`Solving for rectangle dimensions: ${l}, ${w}`);
+
+    rect(l, w, (err, rectangle) => {
+        if(err){
+            console.log('ERROR:', err.message);
+        }else{
+            console.log(`Area of rectangle with dimensions ${l}, ${w} is: ${rectangle.area()}`);
+            console.log(`Perimeter of rectangle with dimensions ${l}, ${w} is: ${rectangle.perimeter()}`);
+        }
+    });
+    console.log('This state is logged after the call to rect()');
+}
 solveRect(2, 4);
 solveRect(3, 5);
 solveRect(0, 5);
